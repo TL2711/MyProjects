38972512eb90e6342f2452d07651b44c93707973
Trung Luu <95296734+TL2711@users.noreply.github.com>
2022-08-31 00:01:33 +0700

    Add files via upload

---
+++ gui/Main.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.gui;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        DNJFrame t = new DNJFrame();
+        t.setVisible(true);
+    }
+    
+}
